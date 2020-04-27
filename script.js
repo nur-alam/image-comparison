0839b60f465c1b8d5863f27e2aa41e0549801ec7
nur alam <nuralam@nur.local>
2020-04-28 02:51:16 +0600

    with es6 syntext

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+// with es6 feature
+var imageComparison = function () {
+  
+    const dragCircles = document.querySelectorAll('.comparison-scrollCircle');
+    const imageComparisonRoot = document.querySelector('.qubely-block-image-comparison');
+    const imageComparisonimages = document.querySelectorAll('.qubely-block-image-comparison img');
+  
+    imageComparisonimages.forEach( function (eachImg) {
+        eachImg.style.width = imageComparisonRoot.offsetWidth + 'px';
+    });
+
+    dragCircles.forEach( function (dragCircle) {
+      
+        const container = dragCircle.parentNode;
+        const resizeElement = container.querySelector('.comparison-resize-img');
+      
+        dragCircle.addEventListener('mousedown', function mouseDownTrigger(event) {
+            // prevent right click 
+            if(event.which == 3 || event.which == 2) {
+                console.log("right click trigger!");
+                dragCircle.removeEventListener('mousedown', mouseDownTrigger);
+                return;
+            }
+            draging(dragCircle);
+        });
+
+        dragCircle.addEventListener('touchmove', function () {
+            moving();
+        });
+      
+        let moving = () => {
+            let pageX = event.pageX ? event.pageX : event.touches[0].clientX,
+                containerOffset = container.getBoundingClientRect().left,
+                containerWidth = container.offsetWidth,
+                movingValue = (pageX  - containerOffset ) / (containerWidth / 100);
+            if(movingValue < 5)
+                movingValue = 5;
+            else if(movingValue > 95)
+                movingValue = 95;
+            dragCircle.style.left = movingValue+'%';
+            resizeElement.style.width = movingValue+'%';
+        };
+      
+        let draging = (dragCircle) => {
+            container.addEventListener('mousemove', moving);
+            const dragRevoveFunc = function (event) {
+                container.removeEventListener('mousemove', moving);
+            };
+            container.addEventListener('mouseup', dragRevoveFunc);
+            window.addEventListener('mouseup', dragRevoveFunc);
+        };
+      
+    }); // end of dragcircle foreach
+  
+};
+
+
+if (
+    document.readyState === 'complete' ||
+    (document.readyState !== 'loading' && !document.documentElement.doScroll)
+) {
+    imageComparison();
+} else {
+    document.addEventListener('DOMContentLoaded', imageComparison);
+}
+
+
+
+
+
+// width jqery
 // $(document).ready(function() {
 //     let dragCircles = $('.comparison-scrollCircle');
 //     let imageComparisonRoot = $('.qubely-block-image-comparison');
@@ -45,105 +116,66 @@
 // });
 
 
-var imageComparison = function () {
-    var dragCircles = document.querySelectorAll('.comparison-scrollCircle');
-    var imageComparisonRoot = document.querySelector('.qubely-block-image-comparison');
-    var imageComparisonimages = document.querySelectorAll('.qubely-block-image-comparison img');
-    imageComparisonimages.forEach( function (eachImg) {
-        eachImg.style.width = imageComparisonRoot.offsetWidth + 'px';
-    });
 
-    dragCircles.forEach( function (dragCircle) {
-        var container = dragCircle.parentNode;
-        var resizeElement = container.querySelector('.comparison-resize-img');
-        dragCircle.addEventListener('mousedown', function mouseDownTrigger(event) {
-            if(event.which == 3 || event.which == 2) {
-                console.log("right click trigger!");
-                dragCircle.removeEventListener('mousedown', mouseDownTrigger);
-                return 0;
-            }
-            draging(dragCircle);
-        });
-        var body = document.body;
-        dragCircle.addEventListener('touchstart', function () {
-            body.style.background = 'yellow';
-        });
-        dragCircle.addEventListener('touchend', function () {
-            body.style.background = 'green';
-        });
-        dragCircle.addEventListener('touchmove', function () {
-            body.style.background = 'blue';
-            moving();
-        });
-        function moving() {
-            let pageX = event.pageX ? event.pageX : event.touches[0].clientX;
-            var containerOffset = container.getBoundingClientRect().left - 40,
-                containerWidth = container.offsetWidth,
-                movingValue = ( ( pageX - 37 )  - containerOffset ) / (containerWidth / 100);
-            if(movingValue < 5)
-                movingValue = 5;
-            else if(movingValue > 95)
-                movingValue = 95;
-            dragCircle.style.left = movingValue+'%';
-            resizeElement.style.width = movingValue+'%';
-        };
-        var draging = (dragCircle) => {
-            container.addEventListener('mousemove', moving);
-            var dragRevoveFunc = function (event) {
-                container.removeEventListener('mousemove', moving);
-            };
-            container.addEventListener('mouseup', dragRevoveFunc);
-            window.addEventListener('mouseup', dragRevoveFunc);
-        };
-    });
-};
+// without es6 systext
+// var imageComparison = function () {
+//     var dragCircles = document.querySelectorAll('.comparison-scrollCircle');
+//     var imageComparisonRoot = document.querySelector('.qubely-block-image-comparison');
+//     var imageComparisonimages = document.querySelectorAll('.qubely-block-image-comparison img');
+//     imageComparisonimages.forEach( function (eachImg) {
+//         eachImg.style.width = imageComparisonRoot.offsetWidth + 'px';
+//     });
 
-if (
-    document.readyState === 'complete' ||
-    (document.readyState !== 'loading' && !document.documentElement.doScroll)
-) {
-    imageComparison();
-} else {
-    document.addEventListener('DOMContentLoaded', imageComparison);
-}
+//     dragCircles.forEach( function (dragCircle) {
+//         var container = dragCircle.parentNode;
+//         var resizeElement = container.querySelector('.comparison-resize-img');
+//         dragCircle.addEventListener('mousedown', function mouseDownTrigger(event) {
+//             if(event.which == 3 || event.which == 2) {
+//                 console.log("right click trigger!");
+//                 dragCircle.removeEventListener('mousedown', mouseDownTrigger);
+//                 return 0;
+//             }
+//             draging(dragCircle);
+//         });
+//         var body = document.body;
+//         dragCircle.addEventListener('touchstart', function () {
+//             body.style.background = 'yellow';
+//         });
+//         dragCircle.addEventListener('touchend', function () {
+//             body.style.background = 'green';
+//         });
+//         dragCircle.addEventListener('touchmove', function () {
+//             body.style.background = 'blue';
+//             moving();
+//         });
+//         function moving() {
+//             let pageX = event.pageX ? event.pageX : event.touches[0].clientX;
+//             var containerOffset = container.getBoundingClientRect().left - 40,
+//                 containerWidth = container.offsetWidth,
+//                 movingValue = ( ( pageX - 37 )  - containerOffset ) / (containerWidth / 100);
+//             if(movingValue < 5)
+//                 movingValue = 5;
+//             else if(movingValue > 95)
+//                 movingValue = 95;
+//             dragCircle.style.left = movingValue+'%';
+//             resizeElement.style.width = movingValue+'%';
+//         };
+//         var draging = (dragCircle) => {
+//             container.addEventListener('mousemove', moving);
+//             var dragRevoveFunc = function (event) {
+//                 container.removeEventListener('mousemove', moving);
+//             };
+//             container.addEventListener('mouseup', dragRevoveFunc);
+//             window.addEventListener('mouseup', dragRevoveFunc);
+//         };
+//     });
+// };
 
-
-
-
-
-
-// const dragCircle = document.querySelector('.comparison-scrollCircle');
-
-// dragCircle.addEventListener('mousedown', (event) => {
-//     console.log(event);
-//     const container = event.target.parentNode;
-//     const resizeElement = container.querySelector('.comparison-resize-img');
-//     draging(container, dragCircle, resizeElement);
-// });
-
-// const draging = (container, dragCircle, resizeElement) => {
-
-//     let moving = () => {
-//         let containerOffset = container.offsetLeft,
-//             containerWidth = container.offsetWidth,
-//             movingValue = (event.pageX - containerOffset) / (containerWidth / 100);
-//         console.log(movingValue);
-//         if(movingValue < 10)
-//             movingValue = 10;
-//         else if(movingValue > 90)
-//             movingValue = 90;
-//         dragCircle.style.left = movingValue+'%';
-//         resizeElement.style.width = movingValue+'%';
-//     }
-
-//     container.addEventListener('mousemove', moving);
-
-//     let dragRevoveFunc = (event) => {
-//         //console.log("drag remove");
-//         container.removeEventListener('mousemove', moving);
-//     }
-
-//     container.addEventListener('mouseup',dragRevoveFunc);
-//     window.addEventListener('mouseup',dragRevoveFunc);
-
+// if (
+//     document.readyState === 'complete' ||
+//     (document.readyState !== 'loading' && !document.documentElement.doScroll)
+// ) {
+//     imageComparison();
+// } else {
+//     document.addEventListener('DOMContentLoaded', imageComparison);
 // }
