b017710ac00346d50f8fa500e97457947d3978ca
nur alam <nuralam@nur.local>
2020-04-27 16:20:15 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,102 @@
+var imageComparison = function () {
+    const dragCircles = document.querySelectorAll('.comparison-scrollCircle');
+    const imageComparisonRoot = document.querySelector('.qubely-block-image-comparison');
+    const imageComparisonimages = document.querySelectorAll('.qubely-block-image-comparison img');
+    imageComparisonimages.forEach( (eachImg) => {
+        eachImg.style.width = imageComparisonRoot.offsetWidth + 'px';
+    });
+
+    dragCircles.forEach( (dragCircle) => {
+        const container = dragCircle.parentNode;
+        const resizeElement = container.querySelector('.comparison-resize-img');
+        dragCircle.addEventListener('mousedown', function mouseDownTrigger(event) {
+            if(event.which == 3 || event.which == 2) {
+                console.log("right click trigger!");
+                dragCircle.removeEventListener('mousedown', mouseDownTrigger);
+                return 0;
+            }
+            draging(dragCircle);
+        });
+        let body = document.body;
+        dragCircle.addEventListener('touchstart', () => {
+            body.style.background = 'yellow';
+        });
+        dragCircle.addEventListener('touchend', () => {
+            body.style.background = 'green';
+        });
+        dragCircle.addEventListener('touchmove', () => {
+            body.style.background = 'blue';
+            moving();
+        });
+        let moving = () => {
+            let pageX = (event.pageX !== undefined) ? event.pageX : event.changedTouches[0].clientX;
+            let containerOffset = container.getBoundingClientRect().left - 40,
+                containerWidth = container.offsetWidth,
+                movingValue = ( ( pageX - 37 )  - containerOffset ) / (containerWidth / 100);
+            if(movingValue < 5)
+                movingValue = 5;
+            else if(movingValue > 95)
+                movingValue = 95;
+            dragCircle.style.left = movingValue+'%';
+            resizeElement.style.width = movingValue+'%';
+        };
+        const draging = (dragCircle) => {
+            container.addEventListener('mousemove', moving);
+            let dragRevoveFunc = (event) => {
+                container.removeEventListener('mousemove', moving);
+            };
+            container.addEventListener('mouseup', dragRevoveFunc);
+            window.addEventListener('mouseup', dragRevoveFunc);
+        };
+    });
+};
+
+if (
+    document.readyState === 'complete' ||
+    (document.readyState !== 'loading' && !document.documentElement.doScroll)
+) {
+    imageComparison();
+} else {
+    document.addEventListener('DOMContentLoaded', imageComparison);
+}
+
+
+
+
+
+
+// const dragCircle = document.querySelector('.comparison-scrollCircle');
+
+// dragCircle.addEventListener('mousedown', (event) => {
+//     console.log(event);
+//     const container = event.target.parentNode;
+//     const resizeElement = container.querySelector('.comparison-resize-img');
+//     draging(container, dragCircle, resizeElement);
+// });
+
+// const draging = (container, dragCircle, resizeElement) => {
+
+//     let moving = () => {
+//         let containerOffset = container.offsetLeft,
+//             containerWidth = container.offsetWidth,
+//             movingValue = (event.pageX - containerOffset) / (containerWidth / 100);
+//         console.log(movingValue);
+//         if(movingValue < 10)
+//             movingValue = 10;
+//         else if(movingValue > 90)
+//             movingValue = 90;
+//         dragCircle.style.left = movingValue+'%';
+//         resizeElement.style.width = movingValue+'%';
+//     }
+
+//     container.addEventListener('mousemove', moving);
+
+//     let dragRevoveFunc = (event) => {
+//         //console.log("drag remove");
+//         container.removeEventListener('mousemove', moving);
+//     }
+
+//     container.addEventListener('mouseup',dragRevoveFunc);
+//     window.addEventListener('mouseup',dragRevoveFunc);
+
+// }
