e14f0f899b8fdac8219fc5f4cef2378acf4df299
nur alam <nuralam@nur.local>
2020-06-03 10:06:31 +0600

    added readme & preview gif

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 
 >## image-comparison vanilla js implementation
 
->Live link [React Todo](https://inspiring-banach-e45933.netlify.com/) to view it in the browser.
+>Live link [image comparison](https://nur-alam.github.io/image-comparison/) to view it in the browser.
+
+---
+>## Preview
+---
+<img src="image-comparison-gif-preview.gif">
--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const imageComparison = function () {
         dragCircle.addEventListener('mousedown', function mouseDownTrigger(event) {
             // prevent right click mousedown event
             if(event.which == 3 || event.which == 2) {
-                dragCircle.removeEventListener('mousedown', mouseDownTrigger); return;
+                dragCircle.removeEventListener('mousedown', mouseDownTrigger); 
+                return;
             }
             draging(dragCircle);
         });
@@ -58,54 +59,3 @@ if (
 } else {
     document.addEventListener('DOMContentLoaded', imageComparison);
 }
-
-
-
-
-
-// width jqery
-// $(document).ready(function() {
-//     let dragCircles = $('.comparison-scrollCircle');
-//     let imageComparisonRoot = $('.qubely-block-image-comparison');
-//     let imageComparisonImages = $('.qubely-block-image-comparison img');
-//     //console.log(imageComparisonImages);
-//     dragCircles.each(function (dragCircle) {
-//         let that = $(this);
-//         let container = $(this).parent();
-//         let resizeElement = container.find('.comparison-resize-img');
-//         //resizeElement.css({"padding" : "10px"});
-//         that.on('mousedown', function (event) {
-//             // console.log(container[0].getBoundingClientRect().left);
-//             draging();
-//         });
-//         that.on('touchmove', function () {
-//             //console.log(container[0].getBoundingClientRect().left);
-//             moving();
-//         });
-//         function moving() {
-//             //console.log(event.pageX)
-//             let pageX = event.pageX ? event.pageX : event.touches[0].clientX,
-//                 containerOffset = container.get(0).getBoundingClientRect().left,
-//                 containerWidth = container.outerWidth(),
-//                 movingValue = (pageX - containerOffset) / (containerWidth / 100);
-//             if(movingValue < 5) movingValue = 5;
-//             else if(movingValue > 95) movingValue = 95;
-//             that.css({"left" : movingValue+"%"});
-//             resizeElement.css({"width" : movingValue+"%"});
-//             //console.log(movingValue,event.touches[0].clientX+" s ", container.get(0).getBoundingClientRect().left);
-//         }
-
-//         function draging() {
-//             container.on('mousemove', moving);
-//             container.on('mouseup', dragRemove);
-//             $(window).on('mouseup', dragRemove);
-//         }
-
-//         function dragRemove() {
-//             container.unbind('mousemove');
-//             console.log('mouse up');
-//         }
-
-//         // console.log(this);
-//     })
-// });
